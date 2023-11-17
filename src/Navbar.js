a94121018c9b438d3db842a287a496e6e30e7392
Guy Richie <qwabs94@gmail.com>
2023-11-17 15:24:59 +0000

    At stage 5 in development of the Escomp Blogpost

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () =>{
+
+    return (
+        <nav className="navbar">
+            <h1>The Escomp Blogposter </h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Create New Blog</a>
+            </div>
+        </nav>
+
+    )
+
+}
+export default Navbar;
