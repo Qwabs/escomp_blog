cd2d3e8f882d15ba730baaac35c93bfdbf4fa2ea
Guy Richie <qwabs94@gmail.com>
2023-12-11 16:55:44 +0000

    created a Custom Hook (useFetch.js)

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
+  const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error,setError] = useState(null);
 
   // const [name, setName] = useState("Richie");
 
@@ -12,27 +13,12 @@ const Home = () => {
   //   setBlogs(newBlogs);
   // };
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setBlogs(data);
-          setIsPending(false);
-        })
-        .catch(err => {
-          console.log(err.message)
-        })
-    }, 1000);
-  }, []);
-
+ 
   return (
     <div className="home">
+      {error && <div> { error } </div>}
       {isPending && <div>Loading....</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      {data && <BlogList data={data} title="All Blogs" />}
 
       {/* <BlogList
         blogs={blogs.filter((blog) => blog.author === "mario")}
