56805cad10388bbd3164bf75b66e6b16841b4fe8
Guy Richie <qwabs94@gmail.com>
2023-12-21 16:58:34 +0000

    Made some changes and added a form entry.

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,21 @@
 const Create = () => {
-    return ( 
-        <div className="create">
-         <h2>Add A New Blog</h2>   
-        </div>
-     );
-}
- 
-export default Create ;
+  return (
+    <div className="create">
+      <h2>Add A New Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input type="text" required />
+        <label>Blog Body:</label>
+        <textarea required/>
+        <label>Blog Author:</label>
+        <select>
+            <option value="Richard">Richard</option>
+            <option value="Richard">Guy Richie</option>
+        </select>
+
+      </form>
+    </div>
+  );
+};
+
+export default Create;
