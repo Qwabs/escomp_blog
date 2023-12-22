c466dee7d0284b2ccfc581341dbdd5271701ad13
Guy Richie <qwabs94@gmail.com>
2023-12-22 17:02:50 +0000

    Added some functionality to the Create Component

--- src/Create.js
+++ src/Create.js
@@ -1,18 +1,46 @@
+import { useState } from "react";
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    fetch("htto://localhost:8000/data/",{
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(blog)
+    }).then(()=> {
+      console.log("New Blog Added")
+    })
+    
+    
+  };
   return (
     <div className="create">
       <h2>Add A New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog Title:</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <label>Blog Body:</label>
-        <textarea required/>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
         <label>Blog Author:</label>
-        <select>
-            <option value="Richard">Richard</option>
-            <option value="Richard">Guy Richie</option>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="">Richard</option>
+          <option value="">Guy Richie</option>
         </select>
-
+        <button>Add Blog</button>
+       
       </form>
     </div>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,22 @@
-import { Link }  from  "react-router-dom"
-const Navbar = () =>{
-
-    return (
-        <nav className="navbar">
-            <h1>The Escomp Blogposter </h1>
-            <div className="links">
-                <Link to="/">Home</Link>
-                <Link to="/create">Create New Blog</Link>
-            </div>
-        </nav>
-
-    )
-
-}
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>The Escomp Blogposter </h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link
+          to="/create"
+          style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px",
+          }}
+        >
+          Create New Blog
+        </Link>
+      </div>
+    </nav>
+  );
+};
 export default Navbar;
